6b8873ba7b1237b01e0363bb7b4d844df003bb1a
Guney Ozsan <guneyozsan@gmail.com>
2021-12-07 03:02:46 +0300

    Fix Android CustomTabs class not found exception on click sign in

--- Assets/AndroidChromeCustomTab.cs
+++ Assets/AndroidChromeCustomTab.cs
@@ -10,7 +10,7 @@ namespace Assets
 #if UNITY_ANDROID
             using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
             using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
-            using (var intentBuilder = new AndroidJavaObject("android.support.customtabs.CustomTabsIntent$Builder"))
+            using (var intentBuilder = new AndroidJavaObject("androidx.browser.customtabs.CustomTabsIntent$Builder"))
             using (var intent = intentBuilder.Call<AndroidJavaObject>("build"))
             using (var uriClass = new AndroidJavaClass("android.net.Uri"))
             using (var uri = uriClass.CallStatic<AndroidJavaObject>("parse", url))
